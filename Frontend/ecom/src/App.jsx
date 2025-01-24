fe470e17c9a3126efc218e3c45c5b433b5e08ada
iiuperboyii <aaronjomon24@gmail.com>
2025-01-24 10:31:30 +0530

    Implemented user signup functionality with email verification.

--- Frontend/ecom/src/App.jsx
+++ Frontend/ecom/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import { Login } from './assets/login'
-const App = () => {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+import SignUp from "./components/signup";
+import Home from "./components/Home";
+
+function App() {
   return (
-   <>
-   <Login></Login>
-   </>
-  )
+    <>
+    {/* <SignUp /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>   
+    </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
